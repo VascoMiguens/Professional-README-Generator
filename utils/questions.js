2b3138f998fa2a28585ca573a8d3c41de29f3392
VascoMiguens <v_miguens@hotmail.com>
2022-11-23 16:55:13 +0000

    - changed questions structure to follow desired order - added async function to repeat question - added validate type to questions

--- utils/questions.js
+++ utils/questions.js
@@ -1,40 +1,157 @@
-module.exports = [
-  {
-    type: "input",
-    message: "Project name: ",
-    name: "title",
+const inquirer = require("inquirer");
+
+
+// Give the Project a name
+const projectName = {
+  type: "input",
+  message: "Project Name: ",
+  name: "title",
+  validate: (answer) => {
+    if (answer) {
+      return true;
+    } else {
+      console.log("Please enter your project name");
+      return false;
+    }
   },
+};
+
+//User selects the format of their description
+const description = [
   {
     type: "list",
-    message: "Description of your project: ",
-    name: "description",
+    message: "Choose an option for the description of your project: ",
+    name: "descriptionChoices",
     choices: ["Text and a list", "Text"],
   },
   {
     tpye: "input",
     message: "Enter your description: ",
     name: "descriptionText",
-    when: (answers) => answers.description === "Text",
   },
+];
+
+//Function that allows the repition of the same question and saves the result in an array
+const list = async (name, items = []) => {
+  console.log(name);
+  const prompts = [
+    {
+      type: "input",
+      name: name,
+      message: "Enter an item to your list: ",
+      validate: (answer) => {
+        if (answer) {
+          return true;
+        } else {
+          console.log("Please enter an item to your list");
+          return false;
+        }
+      },
+    },
+    {
+      type: "confirm",
+      name: "again",
+      message: "Enter another item? ",
+      default: true,
+    },
+  ];
+  // Confirm if the user wants to insert more items and copy the array with the spread syntax
+  const { again, ...answers } = await inquirer.prompt(prompts);
+  //add the new items to the array
+  const newItems = [...items, answers];
+  //Check if the user wants to add another item with "again", if so call list function, if not return the newItems
+  return again ? list(name, newItems) : newItems;
+};
+
+//User selects the format of their installation
+const installation = [
   {
     type: "confirm",
     message: "Does your project require any installation instruction?",
-    name: "installation",
+    name: "installationConfirm",
+  },
+  {
+    type: "list",
+    message: "Choose an option for the installation of your project: ",
+    name: "installationChoices",
+    choices: ["Text", "List"],
+    when: (answers) => answers.installationConfirm == true,
   },
   {
     type: "input",
-    message: "Usage Information:",
-    name: "usage",
+    message: "Installation: ",
+    name: "installationText",
+    when: (answers) => answers.installationChoices == "Text",
+    validate: (answer) => {
+      if (answer) {
+        return true;
+      } else {
+        console.log("Please provide installation instructions");
+        return false;
+      }
+    },
+  },
+];
+
+const remainingQuestions = [
+  {
+    type: "list",
+    message: "Choose an option to demo how to use your project?",
+    name: "usageChoices",
+    choices: ["Gif", "PNG"],
+  },
+  {
+    type: "input",
+    message: "Add a short sentence to introduce your demo: ",
+    name: "usageText",
+    validate: (answer) => {
+      if (answer) {
+        return true;
+      } else {
+        console.log("Please provide a short sentence to introduce your demo");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    message:
+      "What is the name of your demo?(Do not insert the file type, only the name)",
+    name: "demoName",
+    validate: (answer) => {
+      if (answer) {
+        return true;
+      } else {
+        console.log("Please provide the name of your demo");
+        return false;
+      }
+    },
   },
   {
     type: "input",
     message: "How to contribute to your project: ",
     name: "contributing",
+    validate: (answer) => {
+      if (answer) {
+        return true;
+      } else {
+        console.log("Please provide a way to contribute");
+        return false;
+      }
+    },
   },
   {
     type: "input",
     message: "Test instructions: ",
     name: "tests",
+    validate: (answer) => {
+      if (answer) {
+        return true;
+      } else {
+        console.log("Please provide test instructions");
+        return false;
+      }
+    },
   },
   {
     type: "list",
@@ -89,10 +206,34 @@ module.exports = [
     type: "input",
     message: "Github Username: ",
     name: "github",
+    validate: (answer) => {
+      if (answer) {
+        return true;
+      } else {
+        console.log("Please provide a github username");
+        return false;
+      }
+    },
   },
   {
     type: "input",
     message: "Email: ",
     name: "email",
+    validate: (answer) => {
+      if (answer) {
+        return true;
+      } else {
+        console.log("Please provide your email");
+        return false;
+      }
+    },
   },
 ];
+
+module.exports = {
+  projectName,
+  description,
+  installation,
+  remainingQuestions,
+  list,
+};
